d5e698c3a41e349dc986a162b741d65b3f695c8e
Mukta Rani Baishnob <muktabaishnob16@gmail.com>
2025-12-09 04:02:18 +0600

    Added firstpage and firstpageController

---
+++ Doctors-Appointment/src/main/java/com/example/doctorsappointment/firstpageController.java
@@ -0,0 +1,30 @@
+package com.example.doctorsappointment;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+public class firstpageController {
+
+        @FXML private Button btnDoctor;
+
+        @FXML
+        public void initialize() {
+            btnDoctor.setOnAction(e -> goToDoctorPage());
+        }
+
+        private void goToDoctorPage() {
+            try {
+                Stage stage = (Stage) btnDoctor.getScene().getWindow();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/secondpage.fxml"));
+                stage.setScene(new Scene(loader.load()));
+                stage.show();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
